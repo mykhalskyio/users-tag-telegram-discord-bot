f25d6d644397160062c951a9b71e4d212d467944
Oleg Mykhalskyi <xukaro11@gmail.com>
2022-08-16 13:47:28 +0300

    add config

---
+++ internal/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Postgres struct {
+		Port    int    `yaml:"port"`
+		Host    string `yaml:"host"`
+		User    string `yaml:"user"`
+		Pass    string `yaml:"pass"`
+		Name    string `yaml:"name"`
+		Sslmode string `yaml:"sslmode"`
+	} `yaml:"postgres"`
+
+	Telegram struct {
+		Token string `yaml:"token"`
+	} `yaml:"telegram"`
+
+	Discord struct {
+		Token string `yaml:"token"`
+	} `yaml:"discord"`
+}
+
+func GetConfig(path string) (*Config, string, error) {
+	cfg := &Config{}
+	if err := cleanenv.ReadConfig(path, cfg); err != nil {
+		help, _ := cleanenv.GetDescription(cfg, nil)
+		return nil, help, err
+	}
+	return cfg, "", nil
+}
