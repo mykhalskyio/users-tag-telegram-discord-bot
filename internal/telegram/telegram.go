c9577ef79fa85493f3b0c1d62fb341a5c46cf7b7
Oleg Mykhalskyi <xukaro11@gmail.com>
2022-08-20 23:06:28 +0300

    some update

--- internal/telegram/telegram.go
+++ internal/telegram/telegram.go
@@ -1,15 +1,17 @@
 package telegram
 
 import (
+	"log"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/mykhalskyio/users-tag-telegram-discord-bot/internal/entity"
 )
 
 type TelegramStorage interface {
 	Insert(username string, chatId int) error
-	GetAll(chatId int) (*[]entity.ChatUser, error)
+	GetAll(chatId int) (*[]entity.ChatUser, bool, error)
 	Delete(username string, chatId int) error
-	Get(username string, chatId int) (*entity.ChatUser, error)
+	Get(username string, chatId int) (*entity.ChatUser, bool, error)
 }
 
 type TelegramBot struct {
@@ -37,21 +39,37 @@ func (bot *TelegramBot) Start() {
 	updates := bot.Api.GetUpdatesChan(u)
 	for update := range updates {
 		if update.Message != nil {
-			if user, _ := bot.Storage.Get(update.Message.From.UserName, int(update.Message.Chat.ID)); user == nil {
-				bot.Storage.Insert(update.Message.From.UserName, int(update.Message.Chat.ID))
-			}
+			log.Println(update.Message, "dsd", update.Message.From.LastName)
+			/*if update.Message.Text == "" {
+				log.Println("Delete " + update.Message.LeftChatMember.UserName)
+			}*/
+
 			if update.Message.IsCommand() {
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 				switch update.Message.Command() {
 				case "all_chat_users":
-					users, _ := bot.Storage.GetAll(int(update.Message.Chat.ID))
+					users, ok, _ := bot.Storage.GetAll(int(update.Message.Chat.ID))
 					usersStr := ""
-					for _, user := range *users {
-						usersStr += " " + user.Username
+					if ok {
+						for _, user := range *users {
+							usersStr += " " + user.Username
+						}
 					}
 					msg.Text = usersStr
 				}
 				bot.Api.Send(msg)
+			} else {
+				if update.Message.Text == "" {
+					_, ok, _ := bot.Storage.Get(update.Message.From.UserName, int(update.Message.Chat.ID))
+					if ok {
+						log.Println("Delete " + update.Message.LeftChatMember.UserName)
+						bot.Storage.Delete(update.Message.LeftChatMember.UserName, int(update.Message.Chat.ID))
+					}
+				} else {
+					if _, ok, _ := bot.Storage.Get(update.Message.From.UserName, int(update.Message.Chat.ID)); !ok {
+						bot.Storage.Insert(update.Message.From.UserName, int(update.Message.Chat.ID))
+					}
+				}
 			}
 		}
 	}
