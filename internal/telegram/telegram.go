ee6c03be7d17fb4648e619041be6b9500042531b
Oleg Mykhalskyi <xukaro11@gmail.com>
2022-08-25 17:43:35 +0300

    add storage interface and some changes

--- internal/telegram/telegram.go
+++ internal/telegram/telegram.go
@@ -1,37 +1,41 @@
 package telegram
 
 import (
+	"encoding/json"
 	"log"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/mykhalskyio/users-tag-telegram-discord-bot/internal/entity"
+	"github.com/mykhalskyio/users-tag-telegram-discord-bot/pkg/queue"
 )
 
 type TelegramStorage interface {
-	Insert(username string, chatId int) error
-	GetAll(chatId int) (*[]entity.ChatUser, bool, error)
-	Delete(username string, chatId int) error
-	Get(username string, chatId int) (*entity.ChatUser, bool, error)
+	InsertUser(username string, chatId int) error
+	GetAllUsers(chatId int) (*[]entity.ChatUser, bool, error)
+	DeleteUser(username string, chatId int) error
+	GetUser(username string, chatId int) (*entity.ChatUser, bool, error)
 }
 
 type TelegramBot struct {
 	Api     *tgbotapi.BotAPI
+	Queue   *queue.Queue
 	Storage TelegramStorage
 }
 
-func NewTelegramBot(token string, storage TelegramStorage) (*TelegramBot, error) {
+func NewTelegramBot(token string, storage TelegramStorage, queue *queue.Queue) (*TelegramBot, error) {
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		return nil, err
 	}
 	return &TelegramBot{
 		Api:     bot,
+		Queue:   queue,
 		Storage: storage,
 	}, nil
 }
 
 func (bot *TelegramBot) Start() {
-	bot.Api.Debug = true
+	bot.Api.Debug = false
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -39,16 +43,11 @@ func (bot *TelegramBot) Start() {
 	updates := bot.Api.GetUpdatesChan(u)
 	for update := range updates {
 		if update.Message != nil {
-			log.Println(update.Message, "dsd", update.Message.From.LastName)
-			/*if update.Message.Text == "" {
-				log.Println("Delete " + update.Message.LeftChatMember.UserName)
-			}*/
-
 			if update.Message.IsCommand() {
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 				switch update.Message.Command() {
 				case "all_chat_users":
-					users, ok, _ := bot.Storage.GetAll(int(update.Message.Chat.ID))
+					users, ok, _ := bot.Storage.GetAllUsers(int(update.Message.Chat.ID))
 					usersStr := ""
 					if ok {
 						for _, user := range *users {
@@ -56,18 +55,25 @@ func (bot *TelegramBot) Start() {
 						}
 					}
 					msg.Text = usersStr
+				case "ping":
+					msg.Text = "pong!"
+					msgJson, _ := json.Marshal(entity.Message{
+						Text:               "pong!",
+						Discord_channel_id: "channelID",
+					})
+					bot.Queue.SendToQueue(msgJson)
 				}
 				bot.Api.Send(msg)
 			} else {
 				if update.Message.Text == "" {
-					_, ok, _ := bot.Storage.Get(update.Message.From.UserName, int(update.Message.Chat.ID))
+					_, ok, _ := bot.Storage.GetUser(update.Message.From.UserName, int(update.Message.Chat.ID))
 					if ok {
 						log.Println("Delete " + update.Message.LeftChatMember.UserName)
-						bot.Storage.Delete(update.Message.LeftChatMember.UserName, int(update.Message.Chat.ID))
+						bot.Storage.DeleteUser(update.Message.LeftChatMember.UserName, int(update.Message.Chat.ID))
 					}
 				} else {
-					if _, ok, _ := bot.Storage.Get(update.Message.From.UserName, int(update.Message.Chat.ID)); !ok {
-						bot.Storage.Insert(update.Message.From.UserName, int(update.Message.Chat.ID))
+					if _, ok, _ := bot.Storage.GetUser(update.Message.From.UserName, int(update.Message.Chat.ID)); !ok {
+						bot.Storage.InsertUser(update.Message.From.UserName, int(update.Message.Chat.ID))
 					}
 				}
 			}
